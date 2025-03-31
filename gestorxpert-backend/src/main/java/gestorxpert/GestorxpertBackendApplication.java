4bacd53daed341cd516f1e5d0968df5e83136dd6
otaviocode <luizgomesdossantos1970@gmail.com>
2025-03-31 10:29:54 -0300

    Initial Commit

---
+++ gestorxpert-backend/src/main/java/gestorxpert/GestorxpertBackendApplication.java
@@ -0,0 +1,13 @@
+package gestorxpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorxpertBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorxpertBackendApplication.class, args);
+	}
+
+}
---
+++ gestorxpert-backend/src/test/java/com/gestorxpert_backend/GestorxpertBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestorxpert_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorxpertBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
